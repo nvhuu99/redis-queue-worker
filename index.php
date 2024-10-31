dfbda4ab4ae9e5e8ad9ebd8d70e3366375a470d4
huu.nv <nvhuu.business@gmail.com>
2024-10-31 14:55:48 +0700

    Add descriptions

--- index.php
+++ index.php
@@ -6,52 +6,56 @@ use App\Job;
 use App\MessageQueue;
 
 $start = microtime(true);
-echo "Running (10,000 times)..." . PHP_EOL;
 
 Co\run(function () {
+    // Setup the queue
     $queue = (new MessageQueue())
                 ->redis('tcp://redis:6379')
-                ->maxProcess(3)
+                ->maxProcess(3) // set to your desire number (more than 0)
                 ->addWorker('image', new \App\Workers\ImageWorker())
                 ->addWorker('file', new \App\Workers\FileWorker())
+                // ... Add more workers
     ;
 
+    // Start listening
     go(function () use ($queue) {
         $queue->open();
     });
 
-    // Benchmark test with FileWorker
-    // Test write long text 10.000 with 10 process
-    go(function () use ($queue) {
-        for ($i = 0; $i < 10000; ++$i) {
-            $queue->push(Job::fromString('file:test'));
-        }
-
-        $queue->close();
-    });
-    
+    // Usecase 1:
+    // Benchmark test: write long text 10.000 times (please create /tmp before)
     // go(function () use ($queue) {
-    //     echo "Enter commands:" . PHP_EOL;
-    //     while(true) {
-    //         try {
-    //             echo '> ';
-                
-    //             $command = trim(fgets(STDIN));
-    
-    //             if ($command == 'exit') {
-    //                 echo "Exiting: wait for last job to finish..." . PHP_EOL;
-    //                 break;
-    //             }
-    
-    //             $queue->push(Job::fromString($command));
-    //         }
-    //         catch (\Exception $e) {
-    //             echo "Wrong command format. Please try again: " . PHP_EOL;
-    //         }
+    //     for ($i = 0; $i < 10000; ++$i) {
+    //         $queue->push(Job::fromString('file:test'));
     //     }
 
-    //     $queue->close(true);
+    //     $queue->close();
     // });
+    
+    // Usecase 2:
+    // Command enter manually
+    go(function () use ($queue) {
+        echo "Enter commands (use 'exit' to close):" . PHP_EOL;
+        while(true) {
+            try {
+                echo '> ';
+                
+                $command = trim(fgets(STDIN));
+    
+                if ($command == 'exit') {
+                    echo "Exiting: wait for last job to finish..." . PHP_EOL;
+                    break;
+                }
+    
+                $queue->push(Job::fromString($command));
+            }
+            catch (\Exception $e) {
+                echo "Wrong command format. Please try again with the \"worker_label:worker_command\" format" . PHP_EOL;
+            }
+        }
+
+        $queue->close(true);
+    });
 });
 
 echo "Finished after: " . (microtime(true) - $start) . PHP_EOL;
